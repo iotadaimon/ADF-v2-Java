907f4a7506e64d0e5c6886c0d5b6862398e11f86
Mykola Demianko <mykd@protonmail.com>
2020-10-07 17:52:41 +0300

    Add logic and string values in TwoActivitiesChallenge project

--- 2_1-TwoActivitiesChallenge/app/src/main/java/com/example/twoactivitieschallenge/MainActivity.java
+++ 2_1-TwoActivitiesChallenge/app/src/main/java/com/example/twoactivitieschallenge/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.twoactivitieschallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_PASSAGE_ID = "PASSAGE_ID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonOneClick(View view) {
+        launchPassageActivity(0);
+    }
+
+    public void onButtonTwoClick(View view) {
+        launchPassageActivity(1);
+    }
+
+    public void onButtonThreeClick(View view) {
+        launchPassageActivity(2);
+    }
+
+    private void launchPassageActivity(int passageNumber){
+        Intent intent = new Intent(this, PassageActivity.class);
+        intent.putExtra(EXTRA_PASSAGE_ID, passageNumber);
+        startActivity(intent);
     }
 }
--- 2_1-TwoActivitiesChallenge/app/src/main/java/com/example/twoactivitieschallenge/PassageActivity.java
+++ 2_1-TwoActivitiesChallenge/app/src/main/java/com/example/twoactivitieschallenge/PassageActivity.java
@@ -2,7 +2,9 @@ package com.example.twoactivitieschallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PassageActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class PassageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passage);
+
+        TextView scrollViewPassage = findViewById(R.id.scrollview_passage);
+
+        Intent intent = getIntent();
+        int passageID = intent.getIntExtra(MainActivity.EXTRA_PASSAGE_ID, -1);
+        String passage = getResources().getStringArray(R.array.passages)[passageID];
+        scrollViewPassage.setText(passage);
     }
 }
