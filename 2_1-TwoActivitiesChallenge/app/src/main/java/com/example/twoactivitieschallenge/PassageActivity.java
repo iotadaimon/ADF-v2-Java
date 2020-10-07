e275867504f3559cb2f4dc780a369e8777774255
Mykola Demianko <mykd@protonmail.com>
2020-10-07 17:28:01 +0300

    Add passage activity to TwoActivitiesChallenge project

---
+++ 2_1-TwoActivitiesChallenge/app/src/main/java/com/example/twoactivitieschallenge/PassageActivity.java
@@ -0,0 +1,14 @@
+package com.example.twoactivitieschallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PassageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passage);
+    }
+}
