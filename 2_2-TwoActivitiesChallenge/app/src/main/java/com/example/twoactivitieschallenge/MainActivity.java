b231304f9d2b9995283d5cbf691d29354aa72301
Mykola Demianko <mykd@protonmail.com>
2020-10-14 15:25:32 +0300

    Save instance state in MainActivity in TwoActivitiesChallenge Project

--- 2_2-TwoActivitiesChallenge/app/src/main/java/com/example/twoactivitieschallenge/MainActivity.java
+++ 2_2-TwoActivitiesChallenge/app/src/main/java/com/example/twoactivitieschallenge/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.twoactivitieschallenge;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,6 +14,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     public static final int ITEM_REQUEST = 1;
+
+    private static final String SAVED_ITEMS = "saved_items";
 
     private TextView[] items;
     private int pos = 0;
@@ -22,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        items = new TextView[10];
+        items = new TextView[MAX_POS];
         items[0] = findViewById(R.id.textView1);
         items[1] = findViewById(R.id.textView2);
         items[2] = findViewById(R.id.textView3);
@@ -33,6 +37,14 @@ public class MainActivity extends AppCompatActivity {
         items[7] = findViewById(R.id.textView8);
         items[8] = findViewById(R.id.textView9);
         items[9] = findViewById(R.id.textView10);
+
+        if (savedInstanceState != null) {
+            String[] itemNames = savedInstanceState.getStringArray(SAVED_ITEMS);
+
+            for (String item : itemNames) {
+                addItem(item);
+            }
+        }
     }
 
     public void openItemSelectionActivity(View view) {
@@ -60,6 +72,19 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast toast = Toast.makeText(this, "Too many items!", Toast.LENGTH_SHORT);
             toast.show();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        String[] itemNames = new String[pos];
+        if (pos > 0) {
+            for (int i = 0; i < pos; i++) {
+                itemNames[i] = items[i].getText().toString();
+            }
+            outState.putStringArray(SAVED_ITEMS, itemNames);
         }
     }
 }
