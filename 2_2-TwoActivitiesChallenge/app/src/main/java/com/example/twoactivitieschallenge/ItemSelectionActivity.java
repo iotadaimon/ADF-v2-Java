ddc518673733731a8ce6c2c6c6893a31eaab0b90
Mykola Demianko <mykd@protonmail.com>
2020-10-14 11:32:11 +0300

    Add item selection layout to TwoActivitiesChallenge project

---
+++ 2_2-TwoActivitiesChallenge/app/src/main/java/com/example/twoactivitieschallenge/ItemSelectionActivity.java
@@ -0,0 +1,45 @@
+package com.example.twoactivitieschallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ItemSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_selection);
+    }
+
+    public void addItem1(View view) {
+    }
+
+    public void addItem2(View view) {
+    }
+
+    public void addItem3(View view) {
+    }
+
+    public void addItem4(View view) {
+    }
+
+    public void addItem5(View view) {
+    }
+
+    public void addItem6(View view) {
+    }
+
+    public void addItem7(View view) {
+    }
+
+    public void addItem8(View view) {
+    }
+
+    public void addItem9(View view) {
+    }
+
+    public void addItem10(View view) {
+    }
+}
